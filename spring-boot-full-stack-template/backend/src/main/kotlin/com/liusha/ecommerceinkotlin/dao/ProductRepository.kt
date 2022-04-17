fe7647f531501ec316f8f60bf7dff4b2638514ef
liusha <liusha.8852@yahoo.com>
2022-04-17 08:32:47 +0100

    removed spring boot with angular module

--- spring-boot-full-stack-template/backend/src/main/kotlin/com/liusha/ecommerceinkotlin/dao/ProductRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.liusha.ecommerceinkotlin.dao
-
-import com.liusha.ecommerceinkotlin.entity.Product
-import org.springframework.data.jpa.repository.JpaRepository
-
-
-interface ProductRepository: JpaRepository<Product, Long> {}
--- spring-boot-full-stack-template/docker/backend.Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM maven:3.8.2-jdk-11
-
-WORKDIR /app
-COPY ./backend .
-
-CMD ["mvn", "spring-boot:run"]
